2344ebf9f093c503cefd8e6f87000225d981a521
Vinicius <viniagg2@gmail.com>
2025-01-24 22:41:07 -0300

    Add app.py

---
+++ app.py
@@ -0,0 +1,77 @@
+from flask import Flask, request, jsonify
+from models.task import Task
+app = Flask(__name__)
+
+# CRUD
+# Create, Read, Update and Delete = Criar, Ler, Atualizar, Deletar
+# Tabela: Tarefa
+
+tasks = []
+task_id_control = 1
+
+@app.route('/tasks', methods=['POST']) #CRIAR TAREFAS
+def create_task():
+  global task_id_control
+  data = request.get_json()
+  new_task = Task(id=task_id_control, title=data['title'], description=data.get("description", ""))
+  task_id_control += 1
+  tasks.append(new_task)
+  print(tasks)
+  return jsonify({"message": "Nova tarefa criada com sucesso"})
+
+@app.route('/tasks', methods=['GET']) #OBTER TAREFAS
+def get_tasks():
+    task_list = [task.to_dic() for task in tasks]
+    
+    
+
+    output = {
+        "tasks": task_list,
+        "total_tasks": len(task_list)
+
+    }
+
+    return jsonify(output)
+
+@app.route('/tasks/<int:id>', methods=['GET']) #READ
+def get_task(id):
+    for t in tasks:
+        if t.id == id:
+           return jsonify(t.to_dic())   
+
+    return jsonify({"message": "Não foi possivel encontrar atividade"}),  404
+
+
+@app.route('/tasks/<int:id>', methods=["PUT"])
+def update_task(id):
+  task = None
+  for t in tasks:
+    if t.id == id:
+      task = t
+  if task == None:
+    return jsonify({"message": "Não foi possível encontrar a atividade"}), 404
+  data = request.get_json()
+  task.title = data['title']
+  task.description = data['description']
+  task.completed = data['completed']
+  return jsonify({"message": "Tarefa atualizada com sucesso"})
+
+
+@app.route('/tasks/<int:id>', methods=["DELETE"])
+def delete_task(id):
+    task = None
+    for t in tasks:
+     
+     if t.id == id:
+        task = t
+        break
+
+    if not task:
+      return jsonify({"message": "Não foi possível encontrar a atividade"}), 404
+    
+    tasks.remove(task)
+    return jsonify({"message": "Tarefa excluída com sucesso"})
+    
+    
+if __name__ == "__main__":
+  app.run(debug=True)
