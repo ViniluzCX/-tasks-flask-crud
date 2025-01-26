d531210a9aa3906c01d2662d4e7eeaa035c7806f
Vinicius <viniagg2@gmail.com>
2025-01-26 13:23:07 -0300

    Adding Tests and Updating Create endpoint

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def create_task():
   task_id_control += 1
   tasks.append(new_task)
   print(tasks)
-  return jsonify({"message": "Nova tarefa criada com sucesso"})
+  return jsonify({"message": "Nova tarefa criada com sucesso", "id": new_task.id})
 
 @app.route('/tasks', methods=['GET']) #OBTER TAREFAS
 def get_tasks():
@@ -75,3 +75,4 @@ def delete_task(id):
     
 if __name__ == "__main__":
   app.run(debug=True)
+  
